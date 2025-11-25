314875f52ae887b7e996a420c6c32e365bb8e067
Jesse Boonsra <jbb090410@gmail.com>
2025-11-25 12:00:31 +0100

    inleveren

--- 01_functions/Program.cs
+++ 01_functions/Program.cs
@@ -9,8 +9,47 @@ class Program
     }
 
     void Run()
-    {
-        Console.WriteLine("function");
-    }
     //hier komt je function
+    {
+        Console.WriteLine();
+        Vraag1();
+        Vraag2();
+        Vraag3();
+        Vraag4();
+        Vraag5();
+    }
+
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+        void Vraag1()
+    {
+        Console.WriteLine("How long do you think you would survive in a zombie apocalypse?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+    void Vraag2()
+    {
+        Console.WriteLine("What secret conspiracy would you like to actually start letting other people know?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+    void Vraag3()
+    {
+        Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+    void Vraag4()
+    {
+        Console.WriteLine("What would you do if you won a million dollars?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+    void Vraag5()
+    {
+        Console.WriteLine("What is the worst game you played but that you liked anyway?");
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
+#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
 }
