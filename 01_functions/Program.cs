094b3bfaa5a9726c14ec13510680597e16ad0194
Jesse Boonsra <jbb090410@gmail.com>
2025-11-25 14:21:10 +0100

    inleveren

--- 01_functions/Program.cs
+++ 01_functions/Program.cs
@@ -2,6 +2,17 @@
 
 class Program
 {
+    string[] vragen = new string[]
+        {
+            "What 1997 N64 video game features James Bond and is named after the 1995 film?",
+            "What arcade game was called Puckman in Japan?",
+            "How long do you think you would survive in a zombie apocalypse?",
+            "What secret conspiracy would you like to actually start letting other people know?",
+            "If you can still remember, what are your funniest childhood memories?",
+            "What would you do if you won a million dollars?",
+            "What is the worst game you played but that you liked anyway?"
+        };
+
     static void Main(string[] args)
     {
         Program program = new Program();
@@ -9,7 +20,6 @@ class Program
     }
 
     void Run()
-    //hier komt je function
     {
         Console.WriteLine();
         Vraag1();
@@ -17,10 +27,15 @@ class Program
         Vraag3();
         Vraag4();
         Vraag5();
+        string antwoord7 = Vraag7();
+        Console.WriteLine(antwoord7);
+
+        string vraag = GetVraag(0);
+        Console.WriteLine("Extra vraag: " + vraag);
     }
 
-#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
-        void Vraag1()
+
+    void Vraag1()
     {
         Console.WriteLine("How long do you think you would survive in a zombie apocalypse?");
         string antwoord = Console.ReadLine();
@@ -50,6 +65,21 @@ class Program
         string antwoord = Console.ReadLine();
         Console.WriteLine(antwoord);
     }
-#pragma warning restore CS8600 // Converting null literal or possible null value to non-nullable type.
+
+    string Vraag7()
+    {
+        Console.WriteLine("What is the worst game you played but that you liked anyway?");
+        string antwoord = Console.ReadLine();
+        return antwoord;
+
+    }
+
+
+    string GetVraag(int vraagIndex)
+    {
+        return vragen[vraagIndex];
+    }
+
+
 
 }
