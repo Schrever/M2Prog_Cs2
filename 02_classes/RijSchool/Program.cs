c29598bec7c444fcc5d5c202e0be3646973cc03b
Jesse Boonstra <39924@ma-web.nl>
2026-01-26 12:39:29 +0100

    neitaf

---
+++ 02_classes/RijSchool/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace RijSchool;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Run();
+    }
+
+    //x
+    Auto auto1 = new Auto()
+    {
+        automaat = true,
+        kenteken = "AA824H",
+        kilometerStand = 20567,
+        merk = "volkswagen"
+    };
+
+    Auto auto2 = new Auto()
+    {
+        automaat = false,
+        kenteken = "KU786G",
+        kilometerStand = 65739,
+        merk = "volvo"
+    };
+
+    RijLeraar rijLeraar = new RijLeraar()
+    {
+        leeftijd = 30,
+        naam = "John",
+        zzp = false
+    };
+
+    LesUur lesUur = new LesUur()
+    {
+        auto = auto1,
+        rijLeraar = rijLeraar,
+
+        tijd = 1130
+    };
+}
+//ik loop vast ik ga verder als het niet af is ben ik vergeten terug te komen
